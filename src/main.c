0a872ebf381d15134dfa1480a2afd461a946a6c7
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-05-23 21:52:36 +1000

    maybe fixed issue with weird character on stdin

--- src/main.c
+++ src/main.c
@@ -20,28 +20,44 @@ typedef struct Arguments {
 int main(int argc, char ** argv)
 {
     Arguments arguments;
-    if (argc > 10)
-    {
-        fprintf(stderr, "error: not enought arguements!\n");
-        exit(0);
-    }
-    
+    char *arg;
+
     //handle the stdin
-    for (int i=0; i<argc; i++) 
+    for (int i=0; i<argc; i++) //TODO fix issue with null character at end of stdin
     {
+        if (i == argc-1) {
+            arg = argv[i];
+            printf("\narg: %s", arg);
+            
+            
+        }
         if (strcmp(argv[i], "-f") == 0) {
             arguments.filename = argv[i+1];
+            
         } else if (strcmp(argv[i], "-a") == 0) {
             arguments.schedulingAlgorithm = argv[i+1];
-        } else if (strcmp(argv[i], "-s") == 0) {
-            arguments.memoryAllocation = argv[i+1];
+            
         } else if (strcmp(argv[i], "-m") == 0) {
+            arguments.memoryAllocation = argv[i+1];
+            
+        } else if (strcmp(argv[i], "-s") == 0) {
             arguments.memorySize = argv[i+1];
+            
         } else if (strcmp(argv[i], "-q") == 0) {
             arguments.quantum = argv[i+1];
+            
         }
         
-        
+    }
+
+    printf("\n-f : %s", arguments.filename);
+    printf("\n-a : %s", arguments.schedulingAlgorithm);
+    printf("\n-m : %s", arguments.memoryAllocation);
+    printf("\n-s : %s", arguments.memorySize);
+    printf("\n-q : %s", arguments.quantum);
+
+    if (!strcmp(arguments.quantum, "10")) {
+        printf("WOW");
     }
 
     
