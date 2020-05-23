9774be14daf9c9af0522a86f1518c2c5cb423f8d
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-05-23 21:04:53 +1000

    handle the stdin

--- src/main.c
+++ src/main.c
@@ -4,18 +4,45 @@
 #include <strings.h>
 #include "../include/main.h"
 
-//example input: $ ./scheduler -f processes.txt -a ff -s 200 -m p.
+typedef struct Arguments {
+    char* filename;
+    char* schedulingAlgorithm;
+    char* memoryAllocation;
+    char* memorySize;
+    char* quantum;
+} Arguments;
+
+
+
+
+
+//example input: $ ./scheduler -f processes.txt -a ff -s 200 -m p
 int main(int argc, char ** argv)
 {
-    if (argc < 2)
+    Arguments arguments;
+    if (argc > 10)
     {
         fprintf(stderr, "error: not enought arguements!\n");
         exit(0);
     }
-
-    char *filename = argv[2];
-
-    printf("%s", filename);
+    
+    //handle the stdin
+    for (int i=0; i<argc; i++) 
+    {
+        if (strcmp(argv[i], "-f") == 0) {
+            arguments.filename = argv[i+1];
+        } else if (strcmp(argv[i], "-a") == 0) {
+            arguments.schedulingAlgorithm = argv[i+1];
+        } else if (strcmp(argv[i], "-s") == 0) {
+            arguments.memoryAllocation = argv[i+1];
+        } else if (strcmp(argv[i], "-m") == 0) {
+            arguments.memorySize = argv[i+1];
+        } else if (strcmp(argv[i], "-q") == 0) {
+            arguments.quantum = argv[i+1];
+        }
+        
+        
+    }
 
     
     return 0;
